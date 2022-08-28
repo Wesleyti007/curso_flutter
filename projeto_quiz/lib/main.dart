14d9d02e937a166908e6098613991f5d982f12e8
Wesley <wesley@coinfo.com.br>
2022-08-28 01:35:01 -0300

    evolving widget

--- projeto_quiz/lib/main.dart
+++ projeto_quiz/lib/main.dart
@@ -2,14 +2,46 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(new QuestionApp());
+  runApp(QuestionApp());
 }
 
-class QuestionApp extends StatelessWidget{
+class QuestionApp extends StatelessWidget {
 
+  void answer() {
+    print('Pergunta respondida');
+  }
+
+  @override
   Widget build(BuildContext context){
+    final List<String> quiz = [
+      'Qual é a sua cor favorita ?',
+      'Qual o seu animal favorito ?'
+    ];
+
     return MaterialApp(
-      home: Text('Olá flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz'),
+        ),
+        body: Column(
+          children: [
+            Text(quiz[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: answer,
+            )
+          ],
+          
+        ),
+      ),
     );
   }
 }
