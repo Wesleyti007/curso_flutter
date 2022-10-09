0ed6e93c615b38df09a1dc5428ee2c6c12682f2b
Wesley <wesley@coinfo.com.br>
2022-10-08 22:19:05 -0300

    criando classe modelo

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -13,12 +14,36 @@ Widget build(BuildContext context){
 }
 
 class MyHome extends StatelessWidget {
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida',
+     value: 354.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Almoço',
+     value: 50.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de luz',
+     value: 120.12,
+      date: DateTime.now()
+      ),
+
+  ];
 
 Widget build(BuildContext context){
   return Scaffold(
    appBar: AppBar( title: Text('Despesas Pessoais'),
    ),
-   body: Column(children: <Widget>[
+   body: Column(
+     mainAxisAlignment: MainAxisAlignment.spaceAround,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: <Widget>[
     Container(
       width: double.infinity,
       child: Card(
