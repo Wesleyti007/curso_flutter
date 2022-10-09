1f50313ef9dcc59405f1fe65fbb58d8f2568277a
Wesley <wesley@coinfo.com.br>
2022-10-08 22:50:08 -0300

    instalando pacote externo

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,89 +1,84 @@
-
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-
-Widget build(BuildContext context){
-  return MaterialApp(
-    home: MyHome()
-  );
-}
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHome());
+  }
 }
 
 class MyHome extends StatelessWidget {
   final _transaction = [
     Transaction(
-      id: 't1',
-      title: 'Novo tenis de corrida',
-     value: 354.76,
-      date: DateTime.now()
-    ),
+        id: 't1',
+        title: 'Novo tenis de corrida',
+        value: 354.76,
+        date: DateTime.now()),
+    Transaction(id: 't2', title: 'Almoço', value: 50.76, date: DateTime.now()),
     Transaction(
-      id: 't2',
-      title: 'Almoço',
-     value: 50.76,
-      date: DateTime.now()
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta de luz',
-     value: 120.12,
-      date: DateTime.now()
-      ),
-
+        id: 't3', title: 'Conta de luz', value: 120.12, date: DateTime.now()),
   ];
 
-Widget build(BuildContext context){
-  return Scaffold(
-   appBar: AppBar( title: Text('Despesas Pessoais'),
-   ),
-   body: Column(
-     mainAxisAlignment: MainAxisAlignment.spaceAround,
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: <Widget>[
-    Container(
-      width: double.infinity,
-      child: Card(
-        color: Colors.blue,
-        child: Text('Grafico'),
-        elevation: 5,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
       ),
-    ),
-    Column
-    (
-      children:_transaction.map((tr) {
-        return Card(
-          child: Row(children: [
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
             Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Grafico'),
+                elevation: 5,
               ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-              child: Text(tr.value.toString()),
             ),
             Column(
-              children: [
-                Text(tr.title),
-                Text(tr.date.toString())
-              ],
-            )
+              children: _transaction.map((tr) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.grey,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.orange[900]),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(color: Colors.grey[700]),
+                        )
+                      ],
+                    )
+                  ]),
+                );
+              }).toList(),
+            ),
           ]),
-        );
-
-      }).toList(),
-      
-    ),
-
-   ]),
-  );
-}
+    );
+  }
 }
